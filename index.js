bc2f0040528a9bd666281334fa66c64e04717962
krystal <krystal.mark@student.moringaschool.com>
2023-06-19 12:50:27 +0300

    Array filter

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution here
+function findMatching(drivers,name){
+
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+
+}
+
+
+function fuzzyMatch(drivers,letters){
+
+    return drivers.filter(driver => driver.startsWith(letters));
+
+}
+
+
+function matchName(name,){
+
+    for(let i = 0; i < name.length; i++){
+
+        if(nameMatches){
+
+            return true;
+
+        } else {
+
+            return false;
+        }
+    }
+}
